febb675d6c74e2c5032dabeb51c3423c2140637f
Jsbbvk <Jsbbvk@gmail.com>
2017-10-07 16:54:01 -0700

    added Platformer directory

---
+++ Platformer/PlatformJS/PlatformMouseEvents.js
@@ -0,0 +1,14 @@
+function mouseMove(e) {
+
+}
+
+function mouseDown(e) {
+    //console.log("MOUSE DOWN");
+    for (var i = 0; i < drawMenu.length; i++) {
+        var t = drawMenu[i];
+        if (pointRectangle(e.x, e.y, t.x, t.y, t.width, t.height)) {
+            //if (mobile) t.ontouchstart();
+            t.onclick();
+        }
+    }
+}
